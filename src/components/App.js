8e5a732ba37de473be03db249f16cb7b52d88aa1
kenthow@gmail.com <kenthow@gmail.com>
2019-08-24 22:42:58 -0400

    Commit with CV struture

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Main from './Main';
+import Cv from './cv/Cv';
+
+const App = () => {
+    return(
+        <div className='container'>
+            <Main />
+            <Cv />
+        </div>
+    );
+};
+
+export default App;
